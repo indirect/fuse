a5f517726baee6c1dd34769912ba380eb7796180
Andre Arko <andre@arko.net>
2017-07-24 01:03:18 -0700

    move lib into app/lib for autoloading

--- app/lib/github.rb
+++ app/lib/github.rb
@@ -14,14 +14,12 @@ module Github
   end
 
   def self.app
-    require "github/app"
     @app ||= App.new(@app_config)
   end
 
   def self.app_client
     # App bearer tokens are only valid for 10 minutes (minus 10 seconds just in case)
     threaded_memoize(expires_in: 550) do
-      require "github/client"
       Client.new(bearer_token: app.bearer_token)
     end
   end
